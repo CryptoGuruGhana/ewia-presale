e17baa44ca96756d029942b20ca9fab37f8f0a82
CryptoGuruGhana <cryptoguru@ewialabs.xyz>
2023-04-13 14:59:01 +0000

    Navbar, Layout and dashboard files added

---
+++ components/Navbar.tsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react';
+
+const Navbar = () => {
+  const [scrollPos, setScrollPos] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollPos(window.pageYOffset);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav
+      className={`fixed top-0 w-full transition-colors ${
+        scrollPos > 0 ? 'bg-gray-900 text-white' : 'bg-transparent text-gray-900'
+      }`}
+    >
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <div className="flex-shrink-0">
+              <img className="h-8 w-8" src="/logo.svg" alt="Logo" />
+            </div>
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
+                <a href="/" className="nav-link">
+                  Home
+                </a>
+                <a href="/about" className="nav-link">
+                  About
+                </a>
+                <a href="/contact" className="nav-link">
+                  Contact
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ pages/dashboard.tsx
@@ -0,0 +1,14 @@
+import Layout from '../components/Layout';
+
+const Dashboard = () => {
+  return (
+    <Layout>
+      <h1 className="text-4xl font-bold">Welcome to my website!</h1>
+<p className="mt-4 text-xl">
+This is an example of a TypeScript Next.js web app with a responsive sticky navbar that changes background and text color when scrolling down, built using Tailwind CSS.
+</p>
+</Layout>
+);
+};
+
+export default Dashboard;
